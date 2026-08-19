b626d64cc6b2dc9a9bcf7e25e417c2718f11ea33
Dakksel <113535342+Dakksel@users.noreply.github.com>
2026-08-20 03:37:02 +0500

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+/* =====================================================
+   НАСТРОЙКИ FIREBASE — The Шаверма
+   ===================================================== */
+const firebaseConfig = {
+  apiKey:            "AIzaSyDK9tRv2Q6PxP3FOqNqhsFoReO6tqPUgHM",
+  authDomain:         "the-shaverma.firebaseapp.com",
+  projectId:          "the-shaverma",
+  storageBucket:      "the-shaverma.firebasestorage.app",
+  messagingSenderId:  "270119035970",
+  appId:              "1:270119035970:web:b16af260aa64be892ff9db",
+};
+
+firebase.initializeApp(firebaseConfig);
